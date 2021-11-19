8be83b738881aaf06c0cac29d5d1a7c68f7a41be
P728 <pedro_728@live.com>
2021-11-19 07:17:27 -0300

    main

--- Projetos/agenciaViagem/src/agenciaViagem/cadastro.java
+++ Projetos/agenciaViagem/src/agenciaViagem/cadastro.java
@@ -11,30 +11,30 @@ class cadastro {
         PrintStream output = System.out;
         cliente cliente = new cliente();
         viagem viagem = new viagem();
-        String[] dadosCliente = new String[3];
+        String[] dadosCliente = new String[4];
         String[] dadosViagem = new String[5];
 
-        output.print("Seja bem vindo a sua agência de viagens!\n");
+        output.print("\nSeja bem vindo a sua agência de viagens!\n");
 
         for (int i = 0; i < dadosCliente.length; i++) {
             switch (i) {
             case 0 -> {
-                output.println("\nNome: ");
+                output.print("\nNome: ");
                 cliente.setNome(input.next());
                 dadosCliente[i] = cliente.getNome();
             }
             case 1 -> {
-                output.println("Sobrenome: ");
+                output.print("Sobrenome: ");
                 cliente.setSobrenome(input.next());
                 dadosCliente[i] = cliente.getSobrenome();
             }
             case 2 -> {
-                output.println("CPF: ");
+                output.print("CPF: ");
                 cliente.setCPF(input.next());
                 dadosCliente[i] = cliente.getCPF();
             }
             case 3 -> {
-                output.println("Telefone: ");
+                output.print("Telefone: ");
                 cliente.setTel(input.next());
                 dadosCliente[i] = cliente.getTel();
             }
@@ -42,35 +42,35 @@ class cadastro {
             }
             }
         }
-        output.println("Cadastro Realizado!");
-        output.println("\n" + dadosCliente[0] + " " + dadosCliente[1] + ", faça o cadastro da sua viagem.");
+        output.println("\n>>>>>>>>>>>> Cadastro Realizado <<<<<<<<<<<<");
+        output.println("\n" + "Sr(a) " + dadosCliente[0] + " " + dadosCliente[1] + ", faça o cadastro da sua viagem.");
         output.println("Informe os dados abaixo: \n");
 
         int j = 0;
         while (j < dadosViagem.length) {
             switch (j) {
             case 0 -> {
-                output.println("Origem: ");
+                output.print("Origem: ");
                 viagem.setOrigem(input.next());
                 dadosViagem[j] = viagem.getOrigem();
             }
             case 1 -> {
-                output.println("Destino: ");
+                output.print("Destino: ");
                 viagem.setDestino(input.next());
                 dadosViagem[j] = viagem.getDestino();
             }
             case 2 -> {
-                output.println("Dia: ");
+                output.print("Dia: ");
                 viagem.setDia(input.next());
                 dadosViagem[j] = viagem.getDia();
             }
             case 3 -> {
-                output.println("Mês: ");
+                output.print("Mês: ");
                 viagem.setMes(input.next());
                 dadosViagem[j] = viagem.getMes();
             }
             case 4 -> {
-                output.println("Horário: ");
+                output.print("Horário: ");
                 viagem.setHoras(input.next());
                 dadosViagem[j] = viagem.getHoras();
             }
@@ -80,13 +80,15 @@ class cadastro {
             j++;
         }
 
-        output.println("------------------|Dados cliente e viagem|------------------\n" + "Nome: " + dadosCliente[0]
-                + " " + dadosCliente[1] + "\n" + "CPF: " + dadosCliente[2] + " Telefone: " + dadosCliente[3] + "\n"
-                + "Origem: " + dadosViagem[0] + " Destino: " + dadosViagem[1] + "\n" + "Data" + dadosViagem[2] + "/"
-                + dadosViagem[3] + " Horário: " + dadosViagem[4] + "\n"
-                + "------------------------------------------------------------\n");
-        output.println("Cadastro de viagem realizado! Aperte os cintos e se aventure.\n"
-                + "Lembre-se de conferir a data e horario antecipado para se planejar melhor." + "\n" + "\n"
-                + "Agradecemos a prefêrencia, tenha uma ótima viagem!");
+        System.out.println("\n------------------|Dados cliente e viagem|------------------\n" + "\n|     Nome: "
+                + dadosCliente[0] + " " + dadosCliente[1] + "\n" + "|     CPF: " + dadosCliente[2] + " Telefone: "
+                + dadosCliente[3] + "\n" + "|     Origem: " + dadosViagem[0] + " Destino: " + dadosViagem[1] + "\n"
+                + "|     Data: " + dadosViagem[2] + "/" + dadosViagem[3] + " Horário: " + dadosViagem[4] + " horas"
+                + "\n" + "\n------------------------------------------------------------\n");
+        System.out.println("Cadastro de viagem realizado! Aperte os cintos e corra para \na aventura."
+                + " Lembre-se de conferir a data e horario com \nantecedência, para conseguir se planejar." + "\n"
+                + "\n" + "Agradecemos a prefêrencia, tenha uma ótima viagem!");
+        System.out.print("\n          __|__");
+        System.out.println("\n    *---o--(_)--o---*\n");
     }
 }
